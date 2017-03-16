7a1c9eb404da63ad4f8d8ac1a80689d8d13f9a56
Mark Gritter <mgritter@gmail.com>
2017-03-15 23:36:14 -0700

    Added different widths (and support for different heights.) Added a line style that uses rotation. Added more options for line count.

--- app/traceryView/traceryView.js
+++ app/traceryView/traceryView.js
@@ -33,18 +33,48 @@ angular.module('myApp.traceryView', ['ngRoute'])
 	};
 	
 	var CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+	function rangeList( start, end, step ) {
+	    var sb = [];
+	    for ( var i = start; i < end; i += step ) {
+		sb.push( "{" + i + "}" );
+	    }
+	    return "<<" + sb.join( "," ) + ">>";
+	}
 	
 	var metagrammar = {
 	    origin : "{\n#ruleset.standardize#\n}",
 	    ruleset : "#svg##headerfooter##proportion##palette##strokestyle##linedef##lineCount##endpoints#{catchAll} : {}\n",
 	    svg : "{origin} : {((setupEndpoints))((header))((drawLines))((footer))},\n",
-	    headerfooter : "{header} : {<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" height=\"#height#\" width=\"#width#\">},\n{footer} : {</svg>},\n",
-	    proportion : "{height} : {200},\n{width} : {200},\n",
+	    headerfooter : "{header} : {<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" height=\"((height))\" width=\"((width))\">},\n{footer} : {</svg>},\n",
+	    proportion : "#height##width#",
+	    height : [ "{height} : {200},\n" +
+		       "{yOddGrid} : " + rangeList( 1, 20, 2 ) + ",\n" +
+		       "{yEvenGrid} : " + rangeList( 0, 20, 2 ) + ",\n" +
+		       "{yGrid} : " + rangeList( 0, 20, 1 ) + ",\n" ],
+	    
+	    // Several widths available, each with a way of choosing x positions
+	    width : [ "{width} : {200},\n" +
+		      "{xOddGrid} : " + rangeList( 1, 20, 2 ) + ",\n" +
+		      "{xEvenGrid} : " + rangeList( 0, 20, 2 ) + ",\n" +
+		      "{xGrid} : " + rangeList( 0, 20, 1 ) + ",\n",
+		      
+		      "{width} : {360},\n" +
+		      "{xOddGrid} : " + rangeList( 1, 36, 2 ) + ",\n" +
+		      "{xEvenGrid} : " + rangeList( 0, 36, 2 ) + ",\n" +
+		      "{xGrid} : " + rangeList( 0, 36, 1 ) + ",\n",
+
+		      "{width} : {140},\n" +
+		      "{xOddGrid} : " + rangeList( 1, 14, 2 ) + ",\n" +
+		      "{xEvenGrid} : " + rangeList( 0, 14, 2 ) + ",\n" +
+		      "{xGrid} : " + rangeList( 0, 14, 1 ) + ",\n"
+		    ],
+	    
 	    palette : [ "#bichromatic#", "#trichromatic#" ],
 	    color : CSS_COLOR_NAMES,
 	    bichromatic : "{linecolor} : << {#color#}, {#color#} >>,\n",
 	    trichromatic : "{linecolor} : << {#color#}, {#color#}, {#color#} >>,\n",
-	    linedef : "{line} : {<path d=\"((linepath))\" stroke=\"((linecolor))\" stroke-width=\"((linewidth))\"/>},\n",
+	    linedef : "{line} : {<path d=\"((linepath))\" stroke=\"((linecolor))\" stroke-width=\"((linewidth))\" transform=\"((linetransform))\"/>},\n",
 
 	    // Thick lines should be paired with small line count //
 	    strokestyle : [ "[lineCount:#fewLines#]#thicklines#",
@@ -71,40 +101,44 @@ angular.module('myApp.traceryView', ['ngRoute'])
 	    thickAndThin: "{linewidth} : << #thinWidth#, #thickWidth# >>,\n",
 	    
 	    fewLines : "#repeatedDef#{drawLines} : {((tenLines))},\n",
-	    anyNumberOfLines : [ "#repeatedDef#{drawLines} : {((tenLines))},\n",
-				 "#repeatedDef#{drawLines} : {((hundredLines))},\n" ],
+	    anyNumberOfLines :
+	    [ "#repeatedDef#{drawLines} : {((tenLines))},\n",
+	      "#repeatedDef#{drawLines} : {((tenLines))((tenlines))((tenlines))},\n",
+	      "#repeatedDef#{drawLines} : {((hundredLines))},\n",
+	      "#repeatedDef#{drawLines} : {((hundredLines))((hundredLines))},\n" ],
 
 	    repeatedDef : "{tenLines} : {((line))((line))((line))((line))((line))((line))((line))((line))((line))((line))((line))},\n" +
 		"{hundredLines}:  {((tenLines))((tenLines))((tenLines))((tenLines))((tenLines))((tenLines))((tenLines))((tenLines))((tenLines))((tenLines))},\n",
 	    
-	    endpoints : [ "#freeLines#", "#gridLines#", "#fewStart#",
-			  "#fewDeltas#" ],
+	    endpoints : [ "#freeLines#",
+			  "#gridLines#",
+			  "#fewStart#",
+			  "#fewDeltas#",
+			  "#rotatedFixedLength#" ],
 
+	    stdtransform : "{linetransform} : {},\n",
 	    stdpos :  
 	    `
-	    {xpos} : {((r01))((digit))((digit))},
-	    {ypos} : {((r01))((digit))((digit))},
+	    {xpos} : {<<x:((xGrid))((digit))>>((x))},
+	    {ypos} : {<<y:((yGrid))((digit))>>((y))},
 	    {digit} : << {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9} >>,
-	    {r01} : << {0}, {1} >>,
 	    `,
 	    stdsetup : "{setupEndpoints} : {},\n",
 	    stdpath : "{linepath} : {((lineStart)) ((lineEnd))},\n",
 	    stdStart : "{lineStart} : {M((xpos)) ((ypos))},\n",
 	    stdEnd : "{lineEnd} : {L((xpos)) ((ypos))},\n",
 	    
-	    freeLines : "#stdsetup##stdpath##stdpos##stdStart##stdEnd#",	    
-	    gridLines : "#stdsetup##stdpath##gridpos##stdStart##stdEnd#",
+	    freeLines : "#stdsetup##stdtransform##stdpath##stdpos##stdStart##stdEnd#",	    
+	    gridLines : "#stdsetup##stdtransform##stdpath##gridpos##stdStart##stdEnd#",
 
 	    gridpos : `
-	    {xpos} : {((r01))((oddDigit))0},
-	    {ypos} : {((r01))((oddDigit))0},
-	    {oddDigit} : << {1}, {3}, {5}, {7}, {9} >>,
-	    {r01} : << {0}, {1} >>,
+	    {xpos} : {((xOddGrid))0},
+	    {ypos} : {((yOddGrid))0},
 	    `,
 
 	    // Rather than picking a common endpoint, this causes the
 	    // bot to pick an endpoint as part of the 'setup' rule.
-	    fewStart :  "#stdpath##stdpos##stdEnd##fixedPointRules#",
+	    fewStart :  "#stdpath##stdtransform##stdpos##stdEnd##fixedPointRules#",
 	    fixedPointRules : [ "#onePointRule#", "#twoPointRule#",
 				"#threePointRule#" ],
 	    onePointRule : "{setupEndpoints} : {<<sp1:M #xpos# #ypos#>>},\n" +
@@ -115,7 +149,7 @@ angular.module('myApp.traceryView', ['ngRoute'])
 		"{lineStart} : << {((sp1))}, {((sp2))}, {((sp3))} >>,\n",
 
 	    // Same here, the bot picks a delta for each picture.
-	    fewDeltas : "#stdpath##stdpos##stdStart##deltas##deltaEndpointRules#",
+	    fewDeltas : "#stdpath##stdtransform##stdpos##stdStart##deltas##deltaEndpointRules#",
 	    deltas : "{xdelta} : <<{0}, {1((digit))}, {-1((digit))},  {2((digit))}, {-2((digit))}, {3((digit))}, {-3((digit))}>>,\n"+
 		"{ydelta} : {((xdelta))},\n",
 	    
@@ -126,8 +160,16 @@ angular.module('myApp.traceryView', ['ngRoute'])
 	    twoDeltaRule : "{setupEndpoints} : {<<ep1:l #xdelta# #ydelta#>><<ep2:l #xdelta# #ydelta#>>},\n" +
 		"{lineEnd} : << {((ep1))}, {((ep2))} >>,\n",
 	    threeDeltaRule : "{setupEndpoints} : {<<ep1:l #xdelta# #ydelta#>><<ep2:l #xdelta# #ydelta#>><<ep3:l #xdelta# #ydelta#>>},\n" +
-		"{lineEnd} : << {((ep1))}, {((ep2))}, {((ep3))} >>,\n",	    
-	    
+		"{lineEnd} : << {((ep1))}, {((ep2))}, {((ep3))} >>,\n",
+
+	    rotatedFixedLength : "#stdpath##rotateTransform##stdpos##stdStart##deltas##oneDeltaRule#",
+	    rotateTransform : "{linetransform} : {rotate( ((angle)) ((x)) ((y)) )},\n#angleRule#",
+	    angleRule : ["#angleUniform#",
+			 "#angle45#",
+			 "#angle120#"],
+	    angleUniform : "{angle} : " + rangeList( 0, 360, 1 ) + ",\n",
+	    angle45 : "{angle} : " + rangeList( 0, 360, 45 ) + ",\n",
+	    angle120 : "{angle} : " + rangeList( 0, 360, 120 ) + ",\n"
 	}
 	
 	$scope.grammar = tracery.createGrammar( metagrammar );
